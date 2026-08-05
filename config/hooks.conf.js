8edd81ed2280ff9fadbdc9c52e4fa03efa27d83d
vanderlei <derlei1.vanderlei@gmail.com>
2026-08-04 21:29:07 -0300

    Remove terminateApp do BrowserStack

--- config/hooks.conf.js
+++ config/hooks.conf.js
@@ -1,14 +1,9 @@
 export let hooksConf = {
-    // afterSuite: async function() {
-        afterTest: async function (test, context, { error, result, duration, passed, retries }) {
-            await driver.takeScreenshot();
-            await driver.execute('mobile: terminateApp', { bundleId: 'br.com.lojaebac' })
-        },
-        // beforeSuite: async function() {
-        beforeTest: async function() {
-                let state = await driver.queryAppState("br.com.lojaebac")
-            if (state !== 4) {
-                await driver.execute('mobile: launchApp', { bundleId: 'br.com.lojaebac' })
-            }
-        }
+    afterTest: async function () {
+        await driver.takeScreenshot();
+    },
+
+    beforeTest: async function () {
+        let state = await driver.queryAppState('br.com.lojaebac')
+    }
 }
