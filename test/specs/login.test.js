f60ab0a60b1b265f9d6c937b7fa57a88e0fda706
vanderlei <derlei1.vanderlei@gmail.com>
2026-08-04 21:36:04 -0300

    Adiciona pausa para debug

--- test/specs/login.test.js
+++ test/specs/login.test.js
@@ -6,10 +6,19 @@ import profilePage from '../pageobjects/profile.page.js'
 describe('My Login application', () => {
     it('should login with valid credentials', async () => {
         let profileTab = driver.isAndroid ? 'profile' : 'Account'
+
+        await driver.pause(10000) // espera 10 segundos
+
         await homePage.openMenu(profileTab)
-        await loginPage.login('cliente@ebac.art.br', 'GD*peToHNJ1#c$sgk08EaYJQ')
+        await loginPage.login(
+            'cliente@ebac.art.br',
+            'GD*peToHNJ1#c$sgk08EaYJQ'
+        )
+
         await homePage.openMenu(profileTab)
-        expect((await profilePage.profileName('EBAC Cliente')).isDisplayed()).toBeTruthy()
+
+        expect(
+            (await profilePage.profileName('EBAC Cliente')).isDisplayed()
+        ).toBeTruthy()
     })
 })
-
